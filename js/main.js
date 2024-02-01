f8f91347feaad92be31bec40fba80ee2d96c6042
BerMacJrs <145165950+BerMacJrs@users.noreply.github.com>
2024-01-31 19:57:17 -0500

    Complete All Style Import
    Completed All Style Import

--- js/main.js
+++ js/main.js
@@ -1,11 +1,1 @@
 console.log("JS file connected");
-
-const vectorGraphic = document.querySelector('#orange');
-console.log(vectorGraphic);
-
-function logthisId(element) {
-    console.log(this);
-    console.log('clicked on this element:', this.id);
-}
-
-vectorGraphic.addEventListener('click', logthisId);
